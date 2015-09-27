c4d7e8e5b753568519ce52e12dc972e63bf2b2e1
aganezov <aganezov@gwu.edu>
2015-09-27 19:00:58 -0400

    continuation of assembler manager set up with logger set up

--- gos/assembler.py
+++ gos/assembler.py
@@ -1,10 +1,24 @@
 # -*- coding: utf-8 -*-
+import logging
+import os
+from gos.configuration import Configuration
+
 __author__ = "aganezov"
 __email__ = "aganezov(at)gwu.edu"
 __status__ = "development"
 
 
 class AssemblyManager(object):
-    def __init__(self, config=None, logger=None):
-        self.config = config
-        self.logger = logger
+
+    def __init__(self, configuration=None, logger=None, apply_configuration_to_logger=True):
+        self.config = configuration if configuration is not None else Configuration()
+        logging_file_path = os.path.join(
+            self.config[Configuration.OUTPUT][Configuration.OUTPUT_DIRECTORY],
+            self.config[Configuration.LOGGING][Configuration.IO_DESTINATION]
+        )
+        self.logger = logger if logger is not None else logging.getLogger(self.config[Configuration.LOGGING][Configuration.LOGGER_NAME])
+        if apply_configuration_to_logger:
+            handler = logging.FileHandler(logging_file_path)
+            handler.setFormatter(logging.Formatter(self.config[Configuration.LOGGING][Configuration.LOGGING_FORMAT]))
+            self.logger.setLevel(self.config[Configuration.LOGGING][Configuration.LOGGING_LEVEL])
+            self.logger.addHandler(handler)
--- tests/test_assembler.py
+++ tests/test_assembler.py
@@ -1,3 +1,4 @@
+import os
 import unittest
 from gos.assembler import AssemblyManager
 from gos.configuration import Configuration
@@ -8,12 +9,33 @@ __author__ = 'aganezov'
 class AssemblerManagerTestCase(unittest.TestCase):
     def setUp(self):
         self.config = Configuration()
+        self.output_dir_name = self.config[Configuration.OUTPUT][Configuration.OUTPUT_DIRECTORY]
+        try:
+            os.stat(self.output_dir_name)
+        except IOError:
+            os.mkdir(self.output_dir_name)
         self.assembler_manager = AssemblyManager()
+
+    def tearDown(self):
+        try:
+            os.stat(self.output_dir_name)
+            os.rmdir(self.output_dir_name)
+        except IOError:
+            pass
 
     def test_empty_initialization(self):
         manager = AssemblyManager()
-        self.assertIsNone(manager.config)
-        self.assertIsNone(manager.logger)
+        self.assertIsNotNone(manager.config)
+        self.assertIsNotNone(manager.logger)
+
+    def test_default_logger_name(self):
+        self.assertEqual(self.assembler_manager.logger.name,
+                         self.assembler_manager.config[Configuration.LOGGING][Configuration.LOGGER_NAME])
+
+    def test_logger_format(self):
+        self.assertEqual(self.assembler_manager.logger.level,
+                         self.assembler_manager.config[Configuration.LOGGING][Configuration.LOGGING_LEVEL])
+
 
 if __name__ == '__main__':
     unittest.main()
