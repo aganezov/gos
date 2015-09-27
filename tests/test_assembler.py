45145da18f6ee56ac4935319d8c2818b188570fc
aganezov <aganezov@gwu.edu>
2015-09-27 18:27:25 -0400

    name change of test class to reflect what class it tests simple setUP for it implemented

--- tests/test_assembler.py
+++ tests/test_assembler.py
@@ -1,10 +1,15 @@
 import unittest
 from gos.assembler import AssemblyManager
+from gos.configuration import Configuration
 
 __author__ = 'aganezov'
 
 
-class AssemblerTestCase(unittest.TestCase):
+class AssemblerManagerTestCase(unittest.TestCase):
+    def setUp(self):
+        self.config = Configuration()
+        self.assembler_manager = AssemblyManager()
+
     def test_empty_initialization(self):
         manager = AssemblyManager()
         self.assertIsNone(manager.config)
