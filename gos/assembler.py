a8a15dd78ada4f9a1a89db2e60360b24eb18efed
aganezov <aganezov@gwu.edu>
2015-09-28 13:25:39 -0400

    implementation of a exception aware and logging supporting read_gene_order_data method for the assembly manager introduction of a new exception for IO errors during GOS workflow new tests to cover new read_gene_order_data method in assembler manager + rework of tests for logger checking, in order to support DEBUG logging level in test case, but still to use configured test case in real assembly manager usage

--- gos/assembler.py
+++ gos/assembler.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 import logging
+from bg import BreakpointGraph
+from bg.bg_io import GRIMMReader
+from gos.exceptions import GOSIOError
 import os
 from gos.configuration import Configuration
 
@@ -9,7 +12,6 @@ __status__ = "development"
 
 
 class AssemblyManager(object):
-
     def __init__(self, configuration=None, logger=None, apply_configuration_to_logger=True):
         self.config = configuration if configuration is not None else Configuration()
         logging_file_path = os.path.join(
@@ -29,3 +31,38 @@ class AssemblyManager(object):
         self.fragments_data = None
 
         self.logger.info('Assembly manager Initialization')
+
+    def read_gene_order_data(self):
+        self.logger.debug("Creating breakpoint graph to incorporate gene orders")
+        self.breakpoint_graph = BreakpointGraph()
+        silent_io_fail = self.config[Configuration.INPUT][Configuration.GENOMES][Configuration.IO_SILENT_FAIL]
+        merge_edges = self.config[Configuration.INPUT][Configuration.GENOMES][Configuration.MERGE_EDGES]
+
+        self.logger.debug('"merge edges" flag is set to {merge_edges}'.format(merge_edges=merge_edges))
+        self.logger.debug('"silent io fail" flag is set to {silent_io_fail}'.format(silent_io_fail=silent_io_fail))
+        for file_name in self.config[Configuration.INPUT][Configuration.GENOMES][Configuration.SOURCE_FILES]:
+            self.logger.info('Processing gene order file {file_name}'.format(file_name=file_name))
+            try:
+                with open(file_name, mode="rt") as source:
+                    self.breakpoint_graph.update(GRIMMReader.get_breakpoint_graph(source), merge_edges=merge_edges)
+            except ValueError as err:
+                self.logger.error("Error during processing of {file_name}: (error_message)".format(file_name=file_name,
+                                                                                                   error_message=err))
+                self.logger.debug("Silent io fail is set {silent_fail}".format(silent_fail=silent_io_fail))
+                if silent_io_fail:
+                    continue
+                else:
+                    self.logger.critical("Error during processing of gene order file {file_name}. "
+                                         "Silent io fail flag is set to {silent_io_fail}, application can not continue"
+                                         "".format(file_name=file_name, silent_io_fail=silent_io_fail))
+                    raise GOSIOError("Error during processing {file_name} gene order file".format(file_name=file_name))
+            except FileNotFoundError:
+                self.logger.error("No gene order file {file_name} was found".format(file_name=file_name))
+                self.logger.debug("Silent io fail is set {silent_fail}".format(silent_fail=silent_io_fail))
+                if silent_io_fail:
+                    continue
+                else:
+                    self.logger.critical("Error during search for gene order file {file_name}. "
+                                         "Silent io fail flag is set to {silent_io_fail}, application can not continue"
+                                         "".format(file_name=file_name, silent_io_fail=silent_io_fail))
+                    raise GOSIOError("No gene order file {file_name} was found".format(file_name=file_name))
