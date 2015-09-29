68ded20db28b1c491a9acff5bcaefc289d1fc679
aganezov <aganezov@gwu.edu>
2015-09-29 15:40:25 -0400

    GOS-2 Add Executable Strategy Abstract initial take on task implementation

---
+++ tests/test_task.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+import unittest
+
+__author__ = "Sergey Aganezov"
+
+
+class AbstractTaskTestCase(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, False)
+
+
+if __name__ == '__main__':
+    unittest.main()
