18017e89ab0b4dc59ed942df2bd4a8e0c3f3ab79
aganezov <aganezov@gwu.edu>
2015-11-05 23:13:40 -0500

    GOS-6 Design input configuration Initial take on input structure

--- gos/configuration.py
+++ gos/configuration.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-import logging
 
 __author__ = "aganezov"
 __email__ = "aganezov(at)gwu.edu"
@@ -7,4 +6,22 @@ __status__ = "development"
 
 
 class Configuration(dict):
+    """
+    Structure, not yaml, just similar looking. It is just a description.
+
+    input:
+        source:
+            - path: file1_path
+              format: grimm
+              io_silent_fail: input->io_silent_fail
+            - path: file2_path
+            - path: file3_path
+        io_silent_fail: .->io_silent_fail
+        logger: .->logger
+        genomes:                                          # if specified, a check performed to make sure data about all
+                                                          # genomes if present. If not -- retrieved form source files
+            - name: genome1_name
+            - name: genome2_name
+            - name: genome3_name
+    """
     pass
