d34b52700478b19a833df9f659a71f2e7d197c5c
aganezov <aganezov@gwu.edu>
2015-11-07 12:15:37 -0500

    GOS-7 Design algorithm configuration initial take on algorithm configuration

--- gos/configuration.py
+++ gos/configuration.py
@@ -23,5 +23,43 @@ class Configuration(dict):
             - name: genome1_name
             - name: genome2_name
             - name: genome3_name
+
+    algorithm:
+        logger: .->logger
+        tasks:                                            # single processing entity
+            paths: []                                     # unchangeable value is "./tasks",
+                                                          # everything else specified will be appended to "./tasks" directory
+            ...
+        stages:
+            - name: stage1      # must be unique among stages
+              self_loop: false
+              tasks:
+                - task1
+                - task2
+            - name: stage2
+              logger: algorithm->logger
+              self_loop: true
+              tasks:
+                - task2
+            - name: stage3
+              path: path_to_*.py_file                      # if path is specified, the stage is loaded from specified file and
+                                                           # its predefined data is used for initialization
+        rounds:
+            - name: round1
+              self_loop: false
+              logger: algorithm->logger
+              stages:
+                - stage1
+                - stage2
+            - name: round2
+              path: path_to_*.py_file
+
+        pipeline:
+            logger: algorithm->logger
+            self_loop: false
+            rounds:
+                - round1
+                - round2
+                - round1
     """
     pass
