c37caa7860e493bcb91fe426bc3f3c584313cb08
aganezov <aganezov@gwu.edu>
2015-11-20 16:10:19 -0500

    GOS-30 Implemented tasks instantiation
    fixed some naming conventions for tasks in Assembly Manager, making sure that we have a difference between loaded task classes and instantiated tasks

--- gos/assembly_manager.py
+++ gos/assembly_manager.py
@@ -6,8 +6,12 @@ from gos.tasks import TaskLoader
 class AssemblyManager(object):
     def __init__(self, config):
         self.configuration = config
-        self.tasks = {}
+        self.tasks_classes = {}
+        self.tasks_instances = {}
 
     def initiate_tasks(self):
-        self.tasks = TaskLoader().load_tasks(paths=self.configuration[Configuration.ALGORITHM][Configuration.TASKS][Configuration.PATHS])
+        self.tasks_classes = TaskLoader().load_tasks(paths=self.configuration[Configuration.ALGORITHM][Configuration.TASKS][Configuration.PATHS])
+
+    def instantiate_tasks(self):
+        self.tasks_instances = {task_name: task_class() for task_name, task_class in self.tasks_classes.items()}
 
--- tests/test_assembly_manager.py
+++ tests/test_assembly_manager.py
@@ -6,7 +6,7 @@ import unittest
 
 from gos.assembly_manager import AssemblyManager
 from gos.configuration import Configuration
-from gos.tasks import BaseTask
+from gos.tasks import BaseTask, TaskLoader
 from tests.test_tasks import TaskLoaderTestCase
 
 
@@ -39,12 +39,25 @@ class AssemblyManagerTestCase(unittest.TestCase):
         }
         importlib.invalidate_caches()
         self.am.initiate_tasks()
-        self.assertTrue(hasattr(self.am, "tasks"))
-        self.assertTrue(isinstance(self.am.tasks, dict))
+        self.assertTrue(hasattr(self.am, "tasks_classes"))
+        self.assertTrue(isinstance(self.am.tasks_classes, dict))
         for task_name in TaskLoaderTestCase.get_tasks_names():
-            self.assertIn(task_name, self.am.tasks)
-            self.assertTrue(issubclass(self.am.tasks[task_name], BaseTask))
+            self.assertIn(task_name, self.am.tasks_classes)
+            self.assertTrue(issubclass(self.am.tasks_classes[task_name], BaseTask))
 
+    def test_manager_instantiate_tasks(self):
+        self.am.tasks_classes = self.get_tasks_classes()
+        self.am.instantiate_tasks()
+        for task_name in self.am.tasks_classes:
+            self.assertIn(task_name, self.am.tasks_instances)
+            self.assertIsInstance(self.am.tasks_instances[task_name], BaseTask)
+            self.assertIsInstance(self.am.tasks_instances[task_name], self.am.tasks_classes[task_name])
+
+    def get_tasks_classes(self):
+        tmp_files = self.create_correct_temporary_tasks_files()
+        paths = [f.name for f in tmp_files]
+        importlib.invalidate_caches()
+        return TaskLoader().load_tasks(paths=paths)
 
 if __name__ == '__main__':
     unittest.main()
