db3fce1f34277667d624666cd1ab5ad9e6289525
aganezov <aganezov@gwu.edu>
2015-12-07 11:39:31 -0500

    setup.py file update with new description and links

--- setup.py
+++ setup.py
@@ -7,15 +7,15 @@ from gos import version as gos_version
 
 setup(
     name="gos",
-    version=gos_version,
+    version="0.0.0",
     packages=["gos", "tests"],
     install_requires=list(map(lambda entry: entry.strip(), open("requirements.txt", "rt").readlines())),
     author="Sergey Aganezov",
     author_email="aganezov@gwu.edu",
-    description="Gene order based scaffolding tool for multiple genomes",
+    description="Generically organizable supervisor to create multi-level executable pipelines",
     license="LGPLv3",
     keywords=["breakpoint graph", "data structures", "python", "scaffolding", "gene order"],
-    url="https://github.com/sergey-aganezov-jr/gos",
+    url="https://github.com/aganezov/gos",
     classifiers=[
         "Development Status :: 1 - Planning",
         "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
@@ -23,5 +23,6 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5'
     ]
 )
